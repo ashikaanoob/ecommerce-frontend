34792aeec253c12f732e34b4349d6847964afe47
Ashika Ashraf <ashikaashraf19@gmail.com>
2026-04-29 00:24:54 +0530

    https://ecommerce-backend-production-8b1e.up.railway.app/api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecommerce-backend-production-8b1e.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
